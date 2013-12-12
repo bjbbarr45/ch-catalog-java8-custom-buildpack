cef5c623e2eddc3ae653397abf3b3d9eef3cbf26
Mike Youngstrom <youngm@gmail.com>
2013-12-12 10:11:42 -0700

    Change node cf prefix.

--- lib/java_buildpack/framework/app_dynamics.rb
+++ lib/java_buildpack/framework/app_dynamics.rb
@@ -42,9 +42,9 @@ module JavaBuildpack::Framework
       @java_opts << host_name(credentials)
       @java_opts << port(credentials)
       @java_opts << ssl_enabled(credentials)
-      @java_opts << "-Dappdynamics.agent.applicationName='#{sm_credentials['smData']['PortfolioId']}'"
+      @java_opts << "-Dappdynamics.agent.applicationName='#{sm_credentials['smData']['PortfolioName']}'"
       @java_opts << "-Dappdynamics.agent.tierName='#{sm_credentials['smData']['CIName']}'"
-      @java_opts << "-Dappdynamics.agent.nodeName='#{credentials['node-name-prefix']}#{@vcap_application[KEY_NAME]}[$(expr \"$VCAP_APPLICATION\" : '.*instance_index[\": ]*\\([0-9]\\+\\).*')] $(date \"+%Y/%m/%d %H:%M:%S\" --date=\"$(expr \"$VCAP_APPLICATION\" : '.*started_at[\": ]*\"\\([^\"]\\+\\)\".*')\") $(expr \"$VCAP_APPLICATION\" : '.*instance_id[\": ]*\"\\([a-z0-9]\\+\\)\".*')'"
+      @java_opts << "-Dappdynamics.agent.nodeName='[#{credentials['node-name-prefix']}] #{@vcap_application[KEY_NAME]}[$(expr \"$VCAP_APPLICATION\" : '.*instance_index[\": ]*\\([0-9]\\+\\).*')] $(date \"+%Y/%m/%d %H:%M:%S\" --date=\"$(expr \"$VCAP_APPLICATION\" : '.*started_at[\": ]*\"\\([^\"]\\+\\)\".*')\") $(expr \"$VCAP_APPLICATION\" : '.*instance_id[\": ]*\"\\([a-z0-9]\\+\\)\".*')'"
       @java_opts << account_name(credentials)
       @java_opts << account_access_key(credentials)
     end
