bdd174701fb1efc9d6c1720fb6d4929f41466de3
youngstrommj <youngm@gmail.com>
2014-05-13 15:06:40 -0700

    Fix buildpack detection.

--- ci/deploy_buildpack.rb
+++ ci/deploy_buildpack.rb
@@ -27,23 +27,17 @@ Dir["release/*"].each do |file|
   FileUtils.cp("release/#{filename}.#{buildpack_file_suffix}", filename)
 
   puts "Seeing if #{buildpack} already exists"
-  buildpacks = `cf buildpacks`
-  exit $?.exitstatus unless $?.exitstatus == 0
-
-  if buildpacks.include?("\n#{buildpack}")
-    puts "Unlocking buildpack #{buildpack}"
-    system({"HOME" => home_directory}, "cf update-buildpack #{buildpack} --unlock 2>&1")
-    
-    puts "Update buildpack #{buildpack}"
-    system({"HOME" => home_directory}, "cf update-buildpack #{buildpack} -p #{filename} 2>&1")
-    exit $?.exitstatus unless $?.exitstatus == 0
-  else
-    puts "Creating buildpack #{buildpack}"
+  puts "Unlocking buildpack #{buildpack}"
+  system({"HOME" => home_directory}, "cf update-buildpack #{buildpack} --unlock 2>&1")
+  
+  puts "Attempting Update of buildpack #{buildpack}"
+  system({"HOME" => home_directory}, "cf update-buildpack #{buildpack} -p #{filename} 2>&1")
+  if $?.exitstatus != 0
+    puts "Update failed.  Creating #{buildpack} instead"
     system({"HOME" => home_directory}, "cf create-buildpack #{buildpack} #{filename} #{position} 2>&1")
-    exit $?.exitstatus unless $?.exitstatus == 0
   end
   
-  puts "Update buildpack #{ARGV[0]}"
+  puts "Locking buildpack #{ARGV[0]}"
   system({"HOME" => home_directory}, "cf update-buildpack #{buildpack} --lock 2>&1")
   exit $?.exitstatus unless $?.exitstatus == 0
 end
