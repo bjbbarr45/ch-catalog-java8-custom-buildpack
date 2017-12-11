d0a98feeaeee40a66a5e12bc03a70fc56936509f
Mike Youngstrom <youngm@gmail.com>
2017-12-11 15:29:49 -0700

    Update merge script.

--- ci/merge_and_test.rb
+++ ci/merge_and_test.rb
@@ -3,7 +3,7 @@ puts "Checkout branch #{ARGV[0]}"
 system("git checkout -b #{ARGV[0]} origin/#{ARGV[0]} 2>&1")
 exit $?.exitstatus unless $?.exitstatus == 0
 
-puts "Merge branch #{ARGV[0]} with Master"
+puts "Merge branch #{ARGV[0]} with 4.x"
 system("git merge origin/4.x --no-edit 2>&1")
 exit $?.exitstatus unless $?.exitstatus == 0
 
